6cc25ed89696188bad7fcde7450d4b0003331eb2
David Grove Jørgensen <dgj@kb.dk>
2023-03-30 14:30:25 +0200

    first attempt at a complete package

---
+++ web/themes/computerspil/js/computerspill.js
@@ -0,0 +1,21 @@
+/**
+ * @file
+ * ComputerSpill behaviors.
+ */
+
+(function ($, Drupal) {
+
+  'use strict';
+
+  /**
+   * Behavior description.
+   */
+  Drupal.behaviors.computerspill = {
+    attach: function (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (jQuery, Drupal));
